e00eb95bf6ef246956788935ccfb172b4e46f28e
annoor98 <annoor98@hotmail.com>
2020-05-06 17:58:02 -0600

    Updates

---
+++ precache-manifest.792f222e1fb38ac473572591aaa42bf8.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6734c3abd03e4714b2568e58d25e5939",
+    "url": "/react-portfolio-v2/index.html"
+  },
+  {
+    "revision": "7742928819beae657db6",
+    "url": "/react-portfolio-v2/static/css/2.6967f5cf.chunk.css"
+  },
+  {
+    "revision": "b72cee7659fa9556520f",
+    "url": "/react-portfolio-v2/static/css/main.004c73d0.chunk.css"
+  },
+  {
+    "revision": "7742928819beae657db6",
+    "url": "/react-portfolio-v2/static/js/2.628c8968.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/react-portfolio-v2/static/js/2.628c8968.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b72cee7659fa9556520f",
+    "url": "/react-portfolio-v2/static/js/main.ac7c2ee2.chunk.js"
+  },
+  {
+    "revision": "d4773a31db33f9087ef2",
+    "url": "/react-portfolio-v2/static/js/runtime-main.1f27d619.js"
+  },
+  {
+    "revision": "f66d6c4d8543f844741ced751babc95f",
+    "url": "/react-portfolio-v2/static/media/keyboard.f66d6c4d.jpg"
+  },
+  {
+    "revision": "ae76e9e3b8f97372458d00558d4df537",
+    "url": "/react-portfolio-v2/static/media/leaves.ae76e9e3.jpg"
+  },
+  {
+    "revision": "f93062274363784bf93aceb631d8f666",
+    "url": "/react-portfolio-v2/static/media/office.f9306227.jpg"
+  },
+  {
+    "revision": "da2974bdbb16b113083fa331dce989a8",
+    "url": "/react-portfolio-v2/static/media/profilePic.da2974bd.png"
+  }
+]);
